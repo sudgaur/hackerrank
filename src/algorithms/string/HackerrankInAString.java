bc2216b7cf1239fda65f01bc0508b9b8c50624ca
Sudhanshu Gaur <SGaur@advaoptical.com>
2017-05-09 11:21:11 +0530

    Hackerrank in a string solution added

---
+++ src/algorithms/string/HackerrankInAString.java
@@ -0,0 +1,31 @@
+package algorithms.string;
+
+import java.util.Scanner;
+
+public class HackerrankInAString {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int queries = in.nextInt();
+        while (queries-- != 0) {
+            String input = in.next();
+            String hack = "hackerrank";
+            int j = 0;
+            char[] a = input.toCharArray();
+            char[] b = hack.toCharArray();
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] == b[j])
+                    j++;
+                if (j == 10) {
+                    System.out.println("YES");
+                    break;
+                }
+            }
+            if (j != 10)
+                System.out.println("NO");
+        }
+        in.close();
+
+    }
+
+}
